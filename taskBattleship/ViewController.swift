d8f67c2dbe401e43c77706c0c7d74747a72b26a1
Denys <den4iklvivua@gmail.com>
2020-06-01 10:21:34 +0300

    Initial Commit

---
+++ taskBattleship/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskBattleship
+//
+//  Created by MacBook on 6/1/20.
+//  Copyright © 2020 den4iklvivua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
